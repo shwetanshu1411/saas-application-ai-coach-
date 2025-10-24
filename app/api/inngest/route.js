a75bfc8707531a48a285855d19e76ad517b3eb45
shwetanshu1411 <2228149@kiit.ac.in>
2025-10-25 03:28:19 +0530

    commit karne ka man tha

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,8 +1,8 @@
 import { serve } from 'inngest/next';
-import { getInngestClient } from '@/lib/inngest/client';
-import { generateIndustryInsightsWrapper } from '@/lib/inngest/function';
+import { inngestClient } from '@/lib/inngest/client';
+import { generateIndustryInsights } from '@/lib/inngest/function';
 
 export const { GET, POST, PUT } = serve({
-  client: await getInngestClient(),
-  functions: [await generateIndustryInsightsWrapper()],
+  client: inngestClient,
+  functions: [generateIndustryInsights],
 });
